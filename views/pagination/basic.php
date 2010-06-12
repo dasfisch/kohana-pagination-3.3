5e5b015b32011c0a9e15ba8ac0145369320aaa74
Geert De Deckere <geert@idoe.be>
2010-06-12 12:20:25 +0200

    Removed the brackets around the current page number, making it more flexible to style with CSS.

--- views/pagination/basic.php
+++ views/pagination/basic.php
@@ -15,7 +15,7 @@
 	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
 
 		<?php if ($i == $current_page): ?>
-			<strong>[<?php echo $i ?>]</strong>
+			<strong><?php echo $i ?></strong>
 		<?php else: ?>
 			<a href="<?php echo $page->url($i) ?>"><?php echo $i ?></a>
 		<?php endif ?>
