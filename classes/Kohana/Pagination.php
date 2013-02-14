cd70ff792fd5566cd7e3604bed895ecf97289caf
brandon <brandonpao@gmail.com>
2013-02-14 13:29:42 -0500

    uses current url instead of default route for pagination links

--- classes/Kohana/Pagination.php
+++ classes/Kohana/Pagination.php
@@ -222,7 +222,7 @@ class Kohana_Pagination {
 		{
 			case 'query_string':
 			
-				return URL::site($this->_route->uri($this->_route_params).
+				return URL::site($this->_request->url().
 					$this->query(array($this->config['current_page']['key'] => $page)));
 
 			case 'route':
